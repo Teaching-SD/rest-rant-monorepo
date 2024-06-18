bdb92746ab1cb9e1cdfe5cec60d5c8e7080e52fe
Chris DiStasio <stahzz1970@gmail.com>
2024-06-18 13:17:57 -0500

    Adds authentication controller, password migration

---
+++ backend/controllers/authentication.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const db = require("../models")
+const bcrypt = require('bcrypt')
+
+const { User } = db;
+
+router.post('/', async (req, res) => {
+
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ message: `Could not find a user with the provided username and password` })
+    } else {
+        res.json({ user })
+    }
+});
+
+module.exports = router
